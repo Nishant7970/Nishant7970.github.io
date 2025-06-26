dd1e69b0b752a043196ccba3a2e0fa09ccbd749f
Nishant7970 <nk7970922655@gmail.com>
2025-06-26 15:41:13 +0530

    updated

---
+++ MongoDB/q6.js
@@ -0,0 +1,68 @@
+db.employees.find({salary:{$gt:80000}},{_id:0 ,name:1})
+db.employees.find({salary:{$gt:80000}},{_id:0 ,name:1}).limit(2) //only 2 document will visible
+db.employees.find({salary:{$gt:80000}},{_id:0 ,name:1}).limi(2).skip(1) //skip 1 one and 2 document will get visible
+
+ db.employees.countDocuments() //show total no. of document in employees
+
+//aggregation pipeline..
+db.employees.aggregate([
+    {$match:{salary:{$gt:80000}}},
+    {$project:{name:1,salary:1}},
+])
+
+db.employees.aggregate([
+    {$match:{salary:{$gt:80000}}},
+    {$project:{name:1,salary:1}},
+    {$sort:{name:1}}
+])
+
+db.employees.aggregate([
+    {$match:{salary:{$gt:80000}}},
+    {$project:{name:1,salary:1}},
+    {$sort:{name:1}},
+    {$limit:1}
+])
+
+db.employees.aggregate([
+    {$match:{salary:{$gt:80000}}},
+    {$project:{name:1,salary:1,location:1}},
+    {$sort:{name:1}},
+    {$limit:1}
+])
+
+db.employees.aggregate([
+    {$project:{_id:0,name:1,location:1}},
+    {$unwind:"$location"}
+])
+
+db.employees.aggregate([
+    {$project:{_id:0,name:1,salary:1,bonus:{$multiply:["$salary",2]}}},
+    
+])
+
+
+db.employees.aggregate([
+    {$group:{_id:"department",total:{$sum:"$salary"}}},
+    
+])
+db.employees.aggregate([
+    {$group:{_id:null,total:{$sum:"$salary"}}},
+    
+])
+
+
+db.employees.aggregate([
+    {$lookup:{from:"order",localField:"_id",foreignField:"empid",as:"order"}},
+    {$unwind:"$order"}
+])
+db.employees.aggregate([
+    {$lookup:{from:"order",localField:"_id",foreignField:"empid",as:"order"}},
+    {$unwind:"$order"},
+    {$project:{name:1,"order.orderValue":1}}
+])
+db.employees.aggregate([
+    {$lookup:{from:"order",localField:"_id",foreignField:"empid",as:"order"}},
+    {$unwind:"$order"},
+    {$project:{salary:1,name:1,"order.orderValue":1}},
+    {$match:{salary:{$gt:1000}}}
+])
